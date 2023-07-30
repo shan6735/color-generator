eb137b44a635691bdc78bb7557e869833ec3af29
shan <shanabbasi35@gmail.com>
2023-07-30 20:42:52 +0500

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  const [color, setColor] = useState('')
-  const [error, setError] = useState(false)
-  const [list, setList] = useState(new Values('#f15025').all(10))
-
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#f15025").all(10));
+  console.log("CI/CD");
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      let colors = new Values(color).all(10)
-      setList(colors)
+      let colors = new Values(color).all(10);
+      setList(colors);
     } catch (error) {
-      setError(true)
-      console.log(error)
+      setError(true);
+      console.log(error);
     }
-  }
+  };
 
   return (
     <>
-      <section className='container'>
+      <section className="container">
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
           <input
-            type='text'
+            type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder='#f15025'
-            className={`${error ? 'error' : null}`}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
           />
-          <button className='btn' type='submit'>
+          <button className="btn" type="submit">
             submit
           </button>
         </form>
       </section>
-      <section className='colors'>
+      <section className="colors">
         {list.map((color, index) => {
           return (
             <SingleColor
@@ -45,11 +45,11 @@ function App() {
               index={index}
               hexColor={color.hex}
             />
-          )
+          );
         })}
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
